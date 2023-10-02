c0534e1137a0976ee36aaa42285296af9e1f228c
Twoguini <twoguini@gmail.com>
2023-10-01 21:11:42 -0300

    Tenth Commit
    - Turned some parts of the code into functions.

--- Modifier.py
+++ Modifier.py
@@ -3,24 +3,28 @@ import re
 
 #SECTION: Priority List:
 #NOTE: 1° - Finish Functionality
-#NOTE: 2° - Turn Code Into Funcions
+#NOTE: 2° - Recomment Code
 #NOTE: 3° - Test Routine
-#NOTE: 4° - State Variables
+#NOTE: 4° - State All Variables
 #!SECTION
 
+#SECTION: Variables
 #TODO: State all used global variables 
 choosed_Files: str = []
 loop_Controller: int = 0
 file_Names: list = []
 target_Files: list = []
+#!SECTION
 
 # Asks for the File Path
 directory_Path: str = os.path.normpath(input('Insert the path of the target: \n'))
 
+#SECTION: Functions
+
+#SECTION: Function to count files
 def counting_Files(directory_Path: str) -> int:
     ''' Returns a Int with the count of how many files there are into the directory '''
     number_of_Files: int = 0
-    
     # Runs for every file in the dir
     for path in os.listdir(directory_Path):
         
@@ -30,13 +34,72 @@ def counting_Files(directory_Path: str) -> int:
     
     # Returns the output
     return ('Your dir has: {0} files'.format(number_of_Files))
+#!SECTION
 
+#SECTION: Function to store files name and print'em 
+def storing_Files_Name(d_Path, loop_Controller):
+    all_Files_Names = os.scandir(d_Path)
+    for i in all_Files_Names:
+        if (i.is_dir()):loop_Controller += 1
+        else:
+            file_Names.append(os.listdir(directory_Path)[loop_Controller])
+            loop_Controller += 1
+    loop_Controller = 0
+    for i in file_Names:
+        print('*{0} - {1}'.format(loop_Controller+1, file_Names[loop_Controller]))
+        loop_Controller += 1
+#!SECTION
+
+#SECTION: Files range option
+def range_Option(choices, loop_Controller):
+    for i in choices:
+        if ('-' in choices[loop_Controller]):
+            raw_Range_OPT = choices[loop_Controller].split('-')
+            range_OPT_Int = [int(i) for i in raw_Range_OPT]
+            choices.remove(choices[loop_Controller])
+            first_in_Range = min(range_OPT_Int)
+            last_in_Range = max(range_OPT_Int)
+            second_Loop_Controller = 0
+            file_Range = (last_in_Range - first_in_Range)
+            choices.append(str(last_in_Range))
+            while (second_Loop_Controller < file_Range):
+                choices.append(str(first_in_Range))
+                first_in_Range += 1
+                second_Loop_Controller += 1
+        loop_Controller += 1
+#!SECTION
+
+#SECTION: Function to delete blank characteres
+def deleting_Blank(raw_Splitted_Choices, loop_Controller):
+    for i in raw_Splitted_Choices:
+        if (raw_Splitted_Choices[loop_Controller] == ''):
+            raw_Splitted_Choices.remove(raw_Splitted_Choices[loop_Controller])
+            loop_Controller += 1
+#!SECTION
+
+#SECTION: Function to store choices
+def storing_Choices(raw_Splitted_Choices, loop_Controller) -> list:
+    for i in raw_Splitted_Choices:
+        list_Value_Indexer = int(raw_Splitted_Choices[loop_Controller])
+        loop_Controller += 1
+        print(file_Names[list_Value_Indexer-1])
+        target_Files.append(file_Names[list_Value_Indexer-1])
+    return(target_Files)
+#!SECTION
+
+#SECTION: Main function, to modify the files extension
 #TODO: Finish main functionality
-def modify_Extension(target, directory_Path):
+def modify_Extension(target, loop_Controller):
     ''' Main objective! Changes the file extension by changing its after "." charactres '''
-    splitedTargetNameAndExtension = os.path.splitext(directory_Path)
-    name = splitedTargetNameAndExtension[0]
-    
+    for i in target:
+                single_Target = target[loop_Controller]
+                loop_Controller += 1
+    splited_Target_Name_And_Extension = single_Target.split('.')
+    Extension = splited_Target_Name_And_Extension[len(splited_Target_Name_And_Extension)-1]
+    print (Extension)
+#!SECTION
+
+#!SECTION
  
 # Checks if it exists
 # If exists
@@ -57,23 +120,10 @@ if (os.path.exists(directory_Path) != False):
             
         else:
             
-            # Stores the names of those files into a var, only if it is a file.
-            all_Files_Names = os.scandir(directory_Path)
-            for i in all_Files_Names:
-                if (i.is_dir()):loop_Controller += 1
-                else:
-                    file_Names.append(os.listdir(directory_Path)[loop_Controller])
-                    loop_Controller += 1
-            loop_Controller = 0
-            
             # Prints on screen the number os files
             print(counting_Files(directory_Path))
 
-            # lists files 
-            for i in file_Names:
-                print('*{0} - {1}'.format(loop_Controller+1, file_Names[loop_Controller]))
-                loop_Controller += 1
-            loop_Controller = 0
+            storing_Files_Name(directory_Path, loop_Controller = 0)
 
             # Let the user choose which files he wants to modify
             choosed_Files = input('Insert the number of the target: \n(Use : between 2 numbers to indicate a range) \n')
@@ -82,48 +132,19 @@ if (os.path.exists(directory_Path) != False):
             try:
                 
                 # Stores the separated targets into a Array, excluding duplicates
-                raw_Splitted_Choices = list(set(re.split(' |,|-', choosed_Files)))
+                raw_Splitted_Choices = list(set(re.split(' |,|:|;', choosed_Files)))
 
-                #TODO: Turn massive chunks of code, like this one, into as many functions as possible.
+                range_Option(raw_Splitted_Choices, loop_Controller = 0)
 
-                # Identify if there is a : between numbers, if true, target files in range
-                for i in raw_Splitted_Choices:
-                    if (':' in raw_Splitted_Choices[loop_Controller]):
-                        raw_Splited_Range_OPT = raw_Splitted_Choices[loop_Controller].split(':')
-                        splited_Range_OPT_as_Int = [int(i) for i in raw_Splited_Range_OPT]
-                        raw_Splitted_Choices.remove(raw_Splitted_Choices[loop_Controller])
-                        first_File_in_Range = min(splited_Range_OPT_as_Int)
-                        last_File_in_Range = max(splited_Range_OPT_as_Int)
-                        second_Loop_Controller = 0
-                        file_Range = (last_File_in_Range - first_File_in_Range)
-                        raw_Splitted_Choices.append(str(last_File_in_Range))
-                        print(file_Range, last_File_in_Range, first_File_in_Range, raw_Splited_Range_OPT)
-                        while (second_Loop_Controller < file_Range):
-                            raw_Splitted_Choices.append(str(first_File_in_Range))
-                            first_File_in_Range += 1
-                            second_Loop_Controller += 1
-                            print(second_Loop_Controller, file_Range)
-                    loop_Controller += 1
-                loop_Controller = 0
+                deleting_Blank(raw_Splitted_Choices, loop_Controller = 0)
 
-                # Deletes blank cases
-                for i in raw_Splitted_Choices:
-                    if (raw_Splitted_Choices[loop_Controller] == ''):
-                        raw_Splitted_Choices.remove(raw_Splitted_Choices[loop_Controller])
-                        loop_Controller +=1
-                loop_Controller = 0
+                storing_Choices(raw_Splitted_Choices, loop_Controller = 0)
 
-                # Print the target names
-                for i in raw_Splitted_Choices:
-                    list_Value_Indexer = int(raw_Splitted_Choices[loop_Controller])
-                    loop_Controller += 1
-                    print(file_Names[list_Value_Indexer-1])
-                    target_Files.append(file_Names[list_Value_Indexer-1])
-                   
+                modify_Extension(target_Files, loop_Controller = 0)
+                    
             # Case an error occours
             except:
-                print('Use only numbers')
-    
+                print('Invalid Numbers')
          
 # If not
 else: 
